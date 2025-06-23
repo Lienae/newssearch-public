88ff7b0b1f29b331fe3a9467f2e7524c85e69731
elixirel <67626885+elixirel@users.noreply.github.com>
2025-06-23 10:55:32 +0900

    initial commit

---
+++ src/main/java/com/tjeoun/newssearch/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.tjeoun.newssearch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf-> csrf.csrfTokenRepository(new CookieCsrfTokenRepository()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().permitAll())
+                .build();
+    }
+}
---
+++ src/test/java/com/tjeoun/newssearch/NewssearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjeoun.newssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewssearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
