12355344f22cb3a8a36db280604d9cad9f65150a
elixirel <67626885+elixirel@users.noreply.github.com>
2025-07-04 10:11:32 +0900

    이메일 전송
    tjoeun77@gmail.com 계정을 통해 이메일을 전송

---
+++ src/main/java/com/tjeoun/newssearch/dto/MailDto.java
@@ -0,0 +1,20 @@
+package com.tjeoun.newssearch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class MailDto {
+    private String to;
+    private String subject;
+    private String body;
+    private List<MultipartFile> attachments;
+}
---
+++ src/main/java/com/tjeoun/newssearch/util/CustomMultipartFile.java
@@ -0,0 +1,56 @@
+package com.tjeoun.newssearch.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+public class CustomMultipartFile implements MultipartFile {
+    private final byte[] input;
+    private final String fileName;
+    public CustomMultipartFile(byte[] input, String fileName) {
+        this.input = input;
+        this.fileName = fileName;
+
+    }
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return fileName;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return input == null || input.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return input.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return input;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(input);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        try(FileOutputStream fos = new FileOutputStream(dest)) {
+            fos.write(input);
+        }
+    }
+}
