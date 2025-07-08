6a5a0c511d5735810edb090aa8060bae0297fac3
elixirel <67626885+elixirel@users.noreply.github.com>
2025-07-08 15:50:15 +0900

    adminjob
    관리자 할 일 database table 추가 및 관련 코드 추가

---
+++ src/main/java/com/tjeoun/newssearch/repository/AdminJobRepository.java
@@ -0,0 +1,8 @@
+package com.tjeoun.newssearch.repository;
+
+import com.tjeoun.newssearch.entity.AdminJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminJobRepository extends JpaRepository<AdminJob, Long> {
+
+}
