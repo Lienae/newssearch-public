d6eccb1f77c01d95c2152258c6dc3add801424f5
elixirel <67626885+elixirel@users.noreply.github.com>
2025-07-11 14:14:37 +0900

    hotfix
    elasticsearch index 생성 테스트 완료

---
+++ src/main/java/com/tjeoun/newssearch/config/ElasticSearchInitializer.java
@@ -0,0 +1,30 @@
+package com.tjeoun.newssearch.config;
+
+import com.tjeoun.newssearch.document.NewsDocument;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ElasticSearchInitializer implements ApplicationRunner {
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        IndexOperations indexOps = elasticsearchOperations.indexOps(NewsDocument.class);
+        if(indexOps.exists()){
+            System.out.println("deleting existing index" + indexOps.getIndexCoordinates().getIndexName());
+            indexOps.delete();
+        }
+
+        System.out.println("creating new index" + indexOps.getIndexCoordinates().getIndexName());
+        indexOps.create();
+
+        indexOps.putMapping();
+        System.out.println("index initialization complete");
+    }
+}
