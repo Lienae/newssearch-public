bc77cb5534b5fab6ed700981eeef52cbd160bb92
Lienae <ktx408216@gmail.com>
2025-07-14 12:34:41 +0900

    게시글 이미지 업로드 경로 수정

--- src/main/java/com/tjeoun/newssearch/config/WebConfig.java
+++ src/main/java/com/tjeoun/newssearch/config/WebConfig.java
@@ -31,6 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminLogInterceptor)
+
                 .addPathPatterns("/**");
     }
 
